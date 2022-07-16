56c9b3446c937134f3b63266f3d6d785c2c5e856
PrettyCoffee <jonathan-holz@outlook.com>
2022-07-17 01:16:38 +0200

    "Its an easteregg, I swear!"

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -34,6 +34,12 @@ const dummyData: MessageProps[] = [
     timestamp: new Date("2020-02-25T20:37:00.000Z"),
     origin: "bot",
   },
+  {
+    message:
+      "You got me, I am a Malenia simp. Voices in my head won't shut up. Malenia is a good boss yeah yeah, maybe my favorite boss in the game?Yeah sure. THE BEST FROMSOFT BOSS? Fuck it's a good fight why the hell not, the character is cool, the lore too the design as well. The problem is my simp cortex gets activated and thinks 'I could fix her'.",
+    timestamp: new Date("2020-02-25T20:38:00.000Z"),
+    origin: "user",
+  },
 ]
 
 const Layout = styled.div`

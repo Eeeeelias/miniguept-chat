3e8c9f8689e7fc570ee02cb5d99d4b1b1792618a
Eeeeelias <Elias.Albrecht@campus.lmu.de>
2022-07-21 16:37:53 +0200

    added feedback functionality (positive)

--- backend/papi.py
+++ backend/papi.py
@@ -1,3 +1,5 @@
+import os.path
+
 from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin
 import chatty
@@ -6,6 +8,16 @@ app = Flask(__name__)
 CORS(app, resources={"/*": {"origins": "*"}})
 
 models = chatty.collect_models('models')
+model_indices = {}
+for mod in models:
+    if os.path.isfile("data/chats-{}.csv".format(mod)):
+        with open("data/chats-{}.csv".format(mod), 'r') as fp:
+            for count, line in enumerate(fp):
+                pass
+            model_indices[mod] = count
+            fp.close()
+    else:
+        model_indices[mod] = 0
 
 
 # converting the incoming message array to tokens
@@ -39,6 +51,26 @@ def request_handler(bot):
     except:
         return """<img src="https://qph.fs.quoracdn.net/main-qimg-863379f19d3db784b48f4dd78d014c19"
             alt="not available">""", 500
+
+
+@app.route('/feedback', methods=['POST'])
+@cross_origin()
+def feedback_handler():
+    global model_indices
+    content = request.get_json()
+
+    index = model_indices.get(content['model'])
+
+    if content['feedback']:
+        with open("data/chats-{}.csv".format(content['model']), 'a') as f:
+            if index == 0:
+                f.write("index,time,from,line\n")
+            for msg in content['messages']:
+                f.write("{},{},{},{}\n".format(index, msg['timestamp'], msg['origin'], msg['message']))
+                index += 1
+            f.close()
+    model_indices[content['model']] = index
+    return "success", 201
 
 
 if __name__ == '__main__':

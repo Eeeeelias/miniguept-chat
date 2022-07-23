26d2f233dfcd5bef604b0847fe5dfc0a38a58452
PrettyCoffee <jonathan-holz@outlook.com>
2022-07-23 18:06:02 +0200

    Move font to public

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,7 +2,6 @@
 import { render } from "solid-js/web"
 
 import "wicg-inert"
-import "./assets/fonts/quicksand.css"
 import { App } from "./App"
 
 render(() => <App />, document.getElementById("root")!)
